e657b72a6f13091b9591c79caade1dceaddcbb07
grinit10 <grinit10@gmail.com>
2020-04-16 20:29:03 +1000

    Adding basics of flexbox.

--- App.tsx
+++ App.tsx
@@ -1,21 +1,38 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
+import { Text, View } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.tsx to start working on your app !');
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title='Change text' onPress={() => setOutputText('Changed Text')}/>
+    <View style={{ padding: 50, flexDirection:'row', width:'80%', height:300, justifyContent:'space-evenly', alignItems:'stretch' }}>
+      <View
+        style={{
+          backgroundColor: 'red',
+          flex: 3,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>1</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: 'blue',
+          flex: 2,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: 'green',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>3</Text>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
