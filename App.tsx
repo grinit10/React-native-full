09f8f3e34660c682d84b01a93d86bf77fdd97f1f
grinit10 <grinit10@gmail.com>
2020-04-16 20:11:38 +1000

    Adding button and textbox.

--- App.tsx
+++ App.tsx
@@ -1,21 +1,16 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   const [outputText, setOutputText] = useState('Open up App.tsx to start working on your app !');
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title='Change text' onPress={() => setOutputText('Changed Text')}/>
+    <View style={{padding: 50}}>
+      <View style={{flexDirection: 'row', justifyContent:'space-between', alignItems:'center'}}>
+        <TextInput placeholder='Course goal' style={{width:'80%', borderColor: 'black', borderWidth: 1, padding:10}}></TextInput>
+        <Button title='Add'></Button>
+      </View>
+      <View>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
